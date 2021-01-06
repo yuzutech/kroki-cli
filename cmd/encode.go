53f694589ea99b9d39c7ed4fc3925b75c0c2c0c1
Guillaume Grossetie <ggrossetie@gmail.com>
2021-01-06 09:46:19 +0100

    resolves #7 add an encode command (#19)

---
+++ cmd/encode.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/yuzutech/kroki-go"
+)
+
+func Encode(_ *cobra.Command, args []string) {
+	filePath := args[0]
+	if filePath == "-" {
+		reader := bufio.NewReader(os.Stdin)
+		EncodeFromReader(reader)
+	} else {
+		EncodeFromFile(filePath)
+	}
+}
+
+func EncodeFromReader(reader io.Reader) {
+	text, err := GetTextFromReader(reader)
+	if err != nil {
+		exit(err)
+	}
+	result, err := kroki.CreatePayload(text)
+	if err != nil {
+		exit(err)
+	}
+	fmt.Println(result)
+}
+
+func EncodeFromFile(filePath string) {
+	content, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		exit(errors.Wrapf(err, "fail to read file '%s'", filePath))
+	}
+	input := string(content)
+	result, err := kroki.CreatePayload(input)
+	if err != nil {
+		exit(err)
+	}
+	fmt.Println(result)
+}
