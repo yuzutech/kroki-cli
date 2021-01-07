e1b491e599564c634ffac77053f59331fe70b7a8
Guillaume Grossetie <ggrossetie@gmail.com>
2021-01-07 17:42:01 +0100

    resolves #6 add a decode command (#21)

---
+++ cmd/decode_test.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestDecodeFromReader(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "eNpKyUwvSizIUHBXqPZIzcnJ17ULzy_KSakFBAAA__9sQAjG",
+			expected: "digraph G {Hello->World}\n",
+		},
+		{
+			input:    "https://kroki.io/graphviz/svg/eNpKyUwvSizIUHBXqPZIzcnJ17ULzy_KSakFBAAA__9sQAjG",
+			expected: "digraph G {Hello->World}\n",
+		},
+		{
+			input:    "http://localhost:8000/graphviz/svg/eNpKyUwvSizIUHBXqPZIzcnJ17ULzy_KSakFBAAA__9sQAjG",
+			expected: "digraph G {Hello->World}\n",
+		},
+	}
+	for _, c := range cases {
+		buf := bytes.NewBuffer([]byte(""))
+		buf.Write([]byte(c.input))
+		result := CaptureOutput(func() {
+			DecodeFromReader(buf)
+		})
+		if result != c.expected {
+			t.Errorf("DecodeFromReader error\nexpected: %s\nactual:   %s", c.expected, result)
+		}
+	}
+}
